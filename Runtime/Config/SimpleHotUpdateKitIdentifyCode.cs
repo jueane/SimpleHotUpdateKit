1f5c090bb08c83a5678542fa92d95af08c8dfe39
je <je@jueane.com>
2024-05-16 10:25:39 +0800

    auto create SimpleHotUpdateKitIdentifyCode

--- Runtime/Config/SimpleHotUpdateKitIdentifyCode.cs
+++ Runtime/Config/SimpleHotUpdateKitIdentifyCode.cs
@@ -1,5 +1,4 @@
-﻿using System.Collections.Generic;
-using UnityEditor;
+﻿using UnityEditor;
 using UnityEngine;
 
 [CreateAssetMenu(fileName = "SimpleHotUpdateKitIdentifyCode", menuName = "SimpleHotUpdateKitConfig/IdentifyCodeConfig", order = 1)]
@@ -8,9 +7,9 @@ public class SimpleHotUpdateKitIdentifyCode : ScriptableObject
     public string VersionCode;
 
     const string configLoadPath = "SimpleHotUpdateKitConfig/SimpleHotUpdateKitIdentifyCode";
-    private static string configPath = $"Assets/Resources/{configLoadPath}";
+    static readonly string configPath = $"Assets/Resources/{configLoadPath}.asset";
 
-    private static SimpleHotUpdateKitIdentifyCode instance;
+    static SimpleHotUpdateKitIdentifyCode instance;
 
     public static SimpleHotUpdateKitIdentifyCode Instance
     {
@@ -21,7 +20,23 @@ public class SimpleHotUpdateKitIdentifyCode : ScriptableObject
                 instance = Resources.Load<SimpleHotUpdateKitIdentifyCode>(configLoadPath);
                 if (instance == null)
                 {
-                    Debug.LogError($"SimpleHotUpdateKitIdentifyCode not found. Please create a new configuration asset at path {configPath}.");
+                    Debug.LogWarning($"SimpleHotUpdateKitIdentifyCode not found. Creating a new one at path {configPath}.");
+                    instance = CreateInstance<SimpleHotUpdateKitIdentifyCode>();
+
+#if UNITY_EDITOR
+                    string directory = System.IO.Path.GetDirectoryName(configPath);
+                    if (!AssetDatabase.IsValidFolder(directory))
+                    {
+                        string parentDirectory = System.IO.Path.GetDirectoryName(directory);
+                        string newFolder = System.IO.Path.GetFileName(directory);
+                        AssetDatabase.CreateFolder(parentDirectory, newFolder);
+                    }
+
+                    AssetDatabase.CreateAsset(instance, configPath);
+                    AssetDatabase.SaveAssets();
+                    AssetDatabase.Refresh();
+                    Debug.Log($"SimpleHotUpdateKitIdentifyCode created at {configPath}.");
+#endif
                 }
             }
 
@@ -32,11 +47,10 @@ public class SimpleHotUpdateKitIdentifyCode : ScriptableObject
     public void Save()
     {
 #if UNITY_EDITOR
-        Debug.Log($"Save config");
-        // 保存修改到磁盘上
+        Debug.Log("Save config");
         EditorUtility.SetDirty(this);
         AssetDatabase.SaveAssets();
-        AssetDatabase.Refresh(); // 如果需要，刷新资源管理器
+        AssetDatabase.Refresh();
 #endif
     }
 }
